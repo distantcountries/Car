c65c59d5fc33c99be67b569433551ca7eaec2b3d
Dragana Trajkovic <distant.countries@gmail.com>
2019-05-24 20:23:18 +0200

    Commit 5

--- database/migrations/2019_05_24_180948_create_cars_table.php
+++ database/migrations/2019_05_24_180948_create_cars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Cars extends Migration
+class CreateCarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class Cars extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('producer');
+            $table->text('number_of_doors');            
+        });
     }
 
     /**
@@ -23,6 +28,10 @@ class Cars extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('cars');
     }
 }
+
+
+
+
