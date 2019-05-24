4d0f66a6c3ae17edb9e03c397bb857a4ecbb8002
Dragana Trajkovic <distant.countries@gmail.com>
2019-05-24 20:01:30 +0200

    commit 4

---
+++ resources/views/cars/cars.blade.php
@@ -0,0 +1,15 @@
+{{-- @extends('layouts.master')
+
+@section('title', 'List of posts')
+
+@section('content')
+
+    <ul>
+        @foreach ($posts as $post)
+            <li>
+                {{ $post->title }}
+            </li>
+        @endforeach
+    </ul>
+
+@endsection --}}
---
+++ resources/views/cars/show.blade.php
@@ -0,0 +1,16 @@
+{{-- @extends('layouts.master')
+
+@section('title', $post->title)
+
+
+@section('content')
+
+    <h1>
+        {{ $post->title }}
+    </h1>
+    
+    <p>
+        {{ $post->body }}
+    </p>
+
+@endsection --}}
