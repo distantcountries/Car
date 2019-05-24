9a644a0021281cdaeed89ef607198ff94281eb66
Dragana Trajkovic <distant.countries@gmail.com>
2019-05-24 19:38:13 +0200

    Commit 2

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Dragana';
+    $age = 30;
+    return view('welcome', compact('name', 'age' ));
 });
+
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
